9a6e51df5446a41bdfc2c1a52474a5430265348f
Ariel <borochov@student.american.edu>
2013-11-01 14:57:41 -0400

    trying to listen to RTP packets

---
+++ src/RTPMain.java
@@ -0,0 +1,18 @@
+import java.lang.Thread;
+
+
+
+
+public class RTPMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		RTPListener try;
+				
+
+	}
+
+}
